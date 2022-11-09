ea8e16159c671cbc8c93e811dafc9197559d81b0
Adina Lev <adinalev24@gmail.com>
2022-11-09 18:58:48 +0200

    Initial code + Welcome functions

---
+++ Targil0/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace Targil0
+{
+    partial class Program
+    {
+        static void Main(string[] args)
+        {
+            Welcome1464();
+            Welcome3058();
+            Console.ReadKey();
+        }
+
+        static partial void Welcome3058();
+        private static void Welcome1464()
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("{0}, welcome to my first console application", name);
+        }
+
+
