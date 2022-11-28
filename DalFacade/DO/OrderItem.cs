6793215f501d9fc999dcbf5eee236a6d8779b685
Adina Lev <adinalev24@gmail.com>
2022-11-28 22:35:26 +0200

    Progress on Stage 1

--- DalFacade/DO/OrderItem.cs
+++ DalFacade/DO/OrderItem.cs
@@ -8,37 +8,37 @@ namespace DO
 {
     public struct OrderItem
     {
-        public int ID;
-        public int OrderID; // Order's identifier
-        public int ProductID; // Product's identifier
-        public double PPU;
-        public int Quantity;
+        public int ID { get; set; }
+        public int OrderID { get; set; } // Order's identifier
+        public int ProductID { get; set; } // Product's identifier
+        public double Price { get; set; }
+        public int Quantity { get; set; }
         public override string ToString() => $@"
             ID - {ID}
             Order: {OrderID}
             Product: {ProductID}
-            Price Per Unit: {PPU}
-            Quantity: {quantity}
+            Price Per Unit: {Price}
+            Quantity: {Quantity}
         ";
 
-        public int id   // property
-        {
-            get { return ID; }   // get method
-            set { ID = value; }  // set method
-        }
+        //public int id   // property
+        //{
+        //    get { return ID; }   // get method
+        //    set { ID = value; }  // set method
+        //}
 
-        // order identifier and product identifier??
+        //// order identifier and product identifier??
 
-        public double ppu   // property
-        {
-            get { return PPU; }   // get method
-            set { PPU = value; }  // set method
-        }
+        //public double ppu   // property
+        //{
+        //    get { return PPU; }   // get method
+        //    set { PPU = value; }  // set method
+        //}
 
-        public int quantity   // property
-        {
-            get { return Quantity; }   // get method
-            set { Quantity = value; }  // set method
-        }
+        //public int quantity   // property
+        //{
+        //    get { return Quantity; }   // get method
+        //    set { Quantity = value; }  // set method
+        //}
     }
 }
--- DalFacade/DO/Product.cs
+++ DalFacade/DO/Product.cs
@@ -8,11 +8,13 @@ namespace DO
 {
     public struct Product
     {
-        public int ID;
-        public string Name;
-        public double Price;
+        // make constructors for product, order, and orderitem 
+        // create default constructors
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public double Price { get; set; }
         public Enums.Category Category { get; set; } // DO THIS
-        public int InStock;
+        public int InStock { get; set; }
         public override string ToString() => $@"
             Product ID = {ID}: {Name},
             category - {Category} // FIX THIS
@@ -20,31 +22,39 @@ namespace DO
             Amount in stock: {InStock}
         ";
 
-        public int id // property
-        {
-            get { return ID; }   // get method
-            set { ID = value; }  // set method
-        }
-        public string name   // property
-        {
-            get { return Name; }   // get method
-            set { Name = value; }  // set method
-        }
-        public double price   // property
-        {
-            get { return Price; }   // get method
-            set { Price = value; }  // set method
-        }
-        //public enum category   // property
+        //default ctor to assign 0s for everything (right below)
+
+        //public Product()
         //{
-        //    get { return Category; }   // get method
-        //    set { Category = value; }  // set method
+        //    ID = 0;
+        //    Name = "";
         //}
-        public int inStock   // property
-        {
-            get { return InStock; }   // get method
-            set { InStock = value; }  // set method
-        }
+
+        //public int id // property
+        //{
+        //    get { return ID; }   // get method
+        //    set { ID = value; }  // set method
+        //}
+        //public string name   // property
+        //{
+        //    get { return Name; }   // get method
+        //    set { Name = value; }  // set method
+        //}
+        //public double price   // property
+        //{
+        //    get { return Price; }   // get method
+        //    set { Price = value; }  // set method
+        //}
+        ////public enum category   // property
+        ////{
+        ////    get { return Category; }   // get method
+        ////    set { Category = value; }  // set method
+        ////}
+        //public int inStock   // property
+        //{
+        //    get { return InStock; }   // get method
+        //    set { InStock = value; }  // set method
+        //}
 
 
     }
